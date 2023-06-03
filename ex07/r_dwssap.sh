854eb7c2a39d07284be52fe33cb5807e992ba321
Andrii Dokhniak <adokhnia@c3r9s4.42madrid.com>
2023-06-03 10:37:25 +0200

    EX07

--- ex07/r_dwssap.sh
+++ ex07/r_dwssap.sh
@@ -1,2 +1,2 @@
-cat /etc/passwd | awk " NR>=$FT_LINE1 && NR<=$FT_LINE2 " | awk '!/^#/' | awk -F ":" "{print \$1}" | awk '{printf("%s, ",$0)}' | sed 's/\(.*\),/\1./'
+cat /etc/passwd | awk " NR>=$FT_LINE1 && NR<=$FT_LINE2 " | awk '!/^#/' | awk -F ":" "{print \$1}" | rev | sort -r|awk '{printf("%s, ",$0)}' | sed 's/\(.*\),/\1./'
 
