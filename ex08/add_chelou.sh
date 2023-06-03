5a66027e92b91881c2c1388d6d1b273638b50716
Andrii Dokhniak <adokhnia@c3r9s4.42madrid.com>
2023-06-03 12:19:26 +0200

    some fixes

--- ex08/add_chelou.sh
+++ ex08/add_chelou.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-FT_NBR1="\'?\"\\\"'\\"
-b1="'\\\"?!"
-echo $b1 $FT_NBR1
+FT_NBR1=\\\'?\"\\\"\'\\ 
+echo $FT_NBR1
+
+b1=\'?\"\\\"\'\\
+echo $b1
 FT_NBR2=rcrdmddd
 b2=mrdoc
-echo $FT_NBR1 | tr  "\\$b1" 01234
+b3='gtaio luSnemf'
+echo $FT_NBR1 + $FT_NBR2 | tr  $b1 01234 | tr $b2 01234| sed 's/^/ obase=13; ibase=5; /'| bc | tr 0123456789ABC "$b3" 
